da1e08780cb8504ecf6bf3de8b20d9d883125a26
Cataalinm <c.catalinbv@yahoo.com>
2018-12-14 13:50:16 +0200

    done

--- DateTime+String/Program.cs
+++ DateTime+String/Program.cs
@@ -4,12 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Sorry for not creating a class "Person" for this homework to make it cleaner, I did it kind of on the run. 
+//Also I was thinking maybe I should've limited the number of days to 28/30/31(Even go a little further and make february with 29 for leap years) so sorry again. 
+
+
 namespace DateTime_String
 {
     class Program
     {
+
+        public enum Gender
+        {
+            Female,
+            Male
+        }
         static void Main(string[] args)
         {
+
+            int year, month, day;
+            char gender;
+            double age;
+
+            Console.WriteLine("Please enter the day: ");
+            day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the month: ");
+            month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the gender: ");
+            gender = Convert.ToChar(Console.ReadLine());
+
+            DateTime BirthDate = new DateTime(year,month,day);
+            TimeSpan difference = DateTime.Now - BirthDate;
+
+            //Here I took  a few days out because of the leap years. It's not perfect because depending on the year of birth it might substract a day less than it should. 
+            
+            age = difference.Days - difference.Days / 365 / 4;
+            age = age/365;    
+
+            Console.WriteLine("The person is " + (int)age + " years old. ");
+            Console.WriteLine();
+
+            int? gend = null;
+            do
+            {
+                if(gender != 'F' && gender != 'M')
+                {
+                    Console.WriteLine("Wrong gender. Please try again.");
+                    gender = Convert.ToChar(Console.ReadLine()); gend = (int)Gender.Female;
+                }
+                if (gender == 'M')
+                {
+                    gend = (int)Gender.Male;
+                    if (age > 65)
+                       Console.WriteLine("The male person is retired.");
+                    else
+                       Console.WriteLine("The male person will retire at 65 years old whic is in: " + (65 - (int)age) + " years. ");
+                }
+                if(gender == 'F')
+                {
+                    gend = (int)Gender.Female;
+                    if (age > 63)
+                       Console.WriteLine("The female person is retired.");
+                    else
+                       Console.WriteLine("The female person will retire at 63 years old which is in: " + (63 - (int)age) + " years. "); 
+                }
+            } while (gender != 'F' && gender != 'M');
+
+            Console.WriteLine("Press any key to close this program..."); 
+            Console.ReadKey();
         }
     }
 }
---
+++ String/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in anything, just don't leave this empty. ");
+            string empty = Console.ReadLine();
+            if (string.IsNullOrEmpty(empty))
+                Console.WriteLine("You only had one job.. don't leave the string empty :(");
+            else
+                Console.WriteLine("I knew I can count on you not to let this string empty. Well done!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            string firstString = "Marco ", secondString = "POLO";
+            Console.WriteLine("I say " + firstString + "you say " + secondString);
+            Console.WriteLine("Toghether we say " + string.Concat(firstString, secondString));
+            string upperString = firstString.ToUpper();
+            string lowerString = secondString.ToLower();
+            Console.WriteLine("I said we say " + string.Format("{0}{1}", upperString, lowerString));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine(" -----LOTTERY----- ");
+            Console.WriteLine("Enter 6 numbers between 1 and 49 with a space between them");
+            string lottery = Console.ReadLine();
+            string winning = "4 16 29 37 39 44";
+            Console.WriteLine("You numbers are : " + lottery);
+            Console.WriteLine("You are allowed to change one number, if you wish to.");
+            Console.WriteLine("y - Make changes      n - I don't wish to change anything");
+            char choice = Convert.ToChar(Console.ReadLine());
+            if(choice == 'y')
+            {
+                Console.WriteLine("Please enter the number you are willing to change: ");
+                string oldnumber = Console.ReadLine();
+                if (lottery.Contains(oldnumber) == false)
+                {
+                    Console.WriteLine("The number you entered is not one of your old numbers, no change will be made. ");
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the new number: ");
+                    string newnumber = Console.ReadLine();
+                    lottery = lottery.Replace(oldnumber, newnumber);
+                    Console.WriteLine("Your numbers after the change you made are :" + lottery);
+                }
+            }
+
+            Console.WriteLine("The winning numbers are : " + winning);
+            if (string.Compare(lottery, winning) == 0)
+            {
+                Console.WriteLine("Congratulations! You won the lottery. ");
+            }
+            else
+                Console.WriteLine("We are sorry you didn't win today. Better luck next time!");
+            Console.WriteLine("Press any key to close this program...");
+            Console.ReadKey();
+        }
+    }
+}
